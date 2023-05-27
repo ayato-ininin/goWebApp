7bb5f2afcfa98e9e10592f9dc1151ae1a336da48
ayato yamauchi <>
2023-05-27 19:18:54 +0900

    add /loginアクセス時にバリデーション追加

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -44,9 +44,18 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// validate data
+	form := NewForm(r.PostForm)
+	form.Required("email", "password")
+
+	if !form.Valid() {
+		fmt.Fprint(w, "failed validation")
+	}
+
 	// get the username and password from the form
 	email := r.Form.Get("email")
 	password := r.Form.Get("password")
+
 
 	log.Println(email, password)
 
