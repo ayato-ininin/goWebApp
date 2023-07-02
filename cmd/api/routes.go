9edd8abfcc93732e2939ebfd76493db5b9db7223
ayato yamauchi <>
2023-07-02 19:04:01 +0900

    add REST APIのstub作成

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,11 +15,21 @@ func (app *application) routes() http.Handler {
 	// mux.Use(app.enabelCORS)
 
 	// authenication routes - auth handler, reflesh
+	mux.Post("/auth", app.authenticate)
+	mux.Post("/refresh-token", app.refresh)
 
 	// test handler
 
 	// protected routes
+	mux.Route("/users", func(mux chi.Router) {
+		// use auth middleware
 
+		mux.Get("/", app.allUsers)
+		mux.Get("/{userID}", app.getUser)
+		mux.Delete("/{userID}", app.deleteUser)
+		mux.Put("/{userID}", app.insertUser)
+		mux.Patch("/{userID}", app.updateUser)
+	})
 
 	return mux
 }
