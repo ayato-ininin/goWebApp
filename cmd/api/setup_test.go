ac7ad17e2777771956c3a758765a28b9bce74752
ayato yamauchi <>
2023-07-09 10:01:36 +0900

    add test用にexpiredToken生成、定義(go run ./cmd/cli -action=expired)

--- cmd/api/setup_test.go
+++ cmd/api/setup_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 var app application
+var expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiYXVkIjoiZXhhbXBsZS5jb20iLCJleHAiOjE2ODg1MDQzNDIsImlzcyI6ImV4YW1wbGUuY29tIiwibmFtZSI6IkpvaG4gRG9lIiwic3ViIjoiMSJ9.slr7l3MuTHaPzZgvoDLwjJ0DIbUhJAhblE4mGGEp1gs"
 
 // TestMain is the entry point for the test suite
 func TestMain(m *testing.M) {
