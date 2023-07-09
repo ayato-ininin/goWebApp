a6401f7aa7eff47b8deb8852688d97ae06f07638
ayato yamauchi <>
2023-07-09 11:16:00 +0900

    change routesファイルにミドルウェアを

--- cmd/api/api-routes.go
+++ cmd/api/api-routes.go
@@ -12,27 +12,16 @@ func (app *application) routes() http.Handler {
 
 	// register middleware
 	mux.Use(middleware.Recoverer)
-	// mux.Use(app.enabelCORS)
+	mux.Use(app.enableCORS)
 
 	// authenication routes - auth handler, reflesh
 	// Ex.)curl http://localhost:8090/auth -X POST -H "Content-Type:application/json" -d '{"email":"admin@example.com","password":"secret"}'
 	mux.Post("/auth", app.authenticate)
 	mux.Post("/refresh-token", app.refresh)
 
-	// test handler
-	mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		var payload = struct {
-			Message string `json:"message"`
-		}{
-			Message: "Hello, World!",
-		}
-
-		_ = app.writeJSON(w, http.StatusOK, payload)
-	})
-
 	// protected routes
 	mux.Route("/users", func(mux chi.Router) {
-		// use auth middleware
+		mux.Use(app.authRequired)
 
 		mux.Get("/", app.allUsers)
 		mux.Get("/{userID}", app.getUser)
