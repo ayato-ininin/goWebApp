e6313d029cbb67d05c3db028900458f7ef08126c
ayato yamauchi <>
2023-07-09 11:49:51 +0900

    add refresh_tokenの再生成ハンドラー追加

--- cmd/api/api-handlers.go
+++ cmd/api/api-handlers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"errors"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -50,7 +53,66 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	refreshToken := r.Form.Get("refresh_token")
+	claims := &Claims{}
+
+	_, err = jwt.ParseWithClaims(refreshToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(app.JWTSecret), nil
+	})
+
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// refresh tokenの有効期限が30秒以上残っているか確認
+	// 30秒以上残っている場合は、新しいrefresh tokenを発行しない
+	if time.Unix(claims.ExpiresAt.Unix(), 0).Sub(time.Now()) > 30 * time.Second {
+		app.errorJSON(w, errors.New("refresh token does not need renewed yet"), http.StatusBadRequest)
+		return
+	}
+
+	// get the user id from the claims
+	userID, err := strconv.Atoi(claims.Subject)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	user, err := app.DB.GetUser(userID)
+	if err != nil {
+		app.errorJSON(w, errors.New("unknown user"), http.StatusBadRequest)
+		return
+	}
+
+	tokenPairs, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// SPAの場合に使用するらしいが。
+	// 必要な人に応じて、レスポンスに加えてcokkieにもいれておく。
+	// 用途は？
+	http.SetCookie(w, &http.Cookie{
+		Name: "__Host-refresh_token",
+		Path: "/",
+		Value: tokenPairs.RefreshToken,
+		Expires: time.Now().Add(refreshTokenExpiry),
+		MaxAge: int(refreshTokenExpiry.Seconds()),
+		SameSite: http.SameSiteStrictMode,
+		Domain: "localhost",
+		HttpOnly: true,
+		Secure: true,
+	})
+
+	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }
 
 func (app *application) allUsers(w http.ResponseWriter, r *http.Request) {
